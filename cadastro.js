e9a278991e7dd96f3f70130a22f3bc48c02373a6
DanielMonteiro <danielgmnegocio@gmail.com>
2025-12-10 22:38:28 -0300

    adicionando funcionalodade

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,9 @@
 function signUp(email){
     return email;
 }
-
 // pegando o email e cadastrando na plataforna
+function pegaemail(email){
+    return"bem vindo ao sistema";
+}
+    
+// adicionando funcao de menssagem 
